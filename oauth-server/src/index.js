622aa1e770088988328ce91f43d6abec2bb8893e
Paul Ilaga <pgilaga@gmail.com>
2021-10-20 17:32:36 +0800

    Get OAuth code

--- oauth-server/src/index.js
+++ oauth-server/src/index.js
@@ -1,19 +1,35 @@
 const Koa = require('koa');
 const Router = require('@koa/router');
 
+require('dotenv').config();
+
 const app = new Koa();
 const router = new Router();
 
-router.get('/', async ({ response }) => {
+const ZOOM_OAUTH_BASE_URI = 'https://zoom.us/oauth/';
+
+router.get('/', ({ response }) => {
   response.body = 'Hello world!';
+});
+
+router.get('/install', ({ response }) => {
+  response.redirect(
+    `${ZOOM_OAUTH_BASE_URI}authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${process.env.REDIRECT_URI}`,
+  );
+});
+
+router.get('/redirect', ({ response }) => {
+  response.body = 'Success!';
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000, (e) => {
+app.listen(process.env.SERVER_PORT, (e) => {
   if (e) {
     console.error(e);
   } else {
-    console.log(`OAuth server running at http://localhost:${3000}`);
+    console.log(
+      `OAuth server running at http://localhost:${process.env.SERVER_PORT}`,
+    );
   }
 });
