51c21ca8d9edbb48ffdc9507cb525ae2ed934129
Paul Ilaga <pgilaga@gmail.com>
2021-10-28 18:28:05 +0800

    Fix converter test name

--- src/steps/groups/converters.test.ts
+++ src/steps/groups/converters.test.ts
@@ -1,7 +1,7 @@
 import { getMockGroup } from '../../../test/mocks';
 import { createGroupEntity } from './converters';
 
-describe('#createUserEntity', () => {
+describe('#createGroupEntity', () => {
   test('should convert to entity', () => {
     expect(createGroupEntity(getMockGroup())).toMatchSnapshot();
   });
