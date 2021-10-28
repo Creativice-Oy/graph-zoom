7eac7a18cbecdc51ff3bdd6d4a92af2ef547f886
Paul Ilaga <pgilaga@gmail.com>
2021-10-28 12:49:08 +0800

    Remove console log

--- src/steps/groups/index.ts
+++ src/steps/groups/index.ts
@@ -18,7 +18,6 @@ export async function fetchGroups({
   const apiClient = createAPIClient(instance.config);
 
   await apiClient.iterateGroups(async (group) => {
-    console.log('group', group);
     await jobState.addEntity(createGroupEntity(group));
   });
 }
