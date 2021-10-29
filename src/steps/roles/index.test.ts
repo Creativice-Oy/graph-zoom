5021c75dd631fa9fafd6bce4fcc230b78a85d358
Paul Ilaga <pgilaga@gmail.com>
2021-10-29 13:42:53 +0800

    Add tests

--- src/steps/roles/index.test.ts
+++ src/steps/roles/index.test.ts
@@ -3,7 +3,7 @@ import {
   Recording,
 } from '@jupiterone/integration-sdk-testing';
 import { IntegrationConfig } from '../../config';
-import { buildUserAndGroupsRelationship, fetchRoles } from '.';
+import { buildUserAndRolesRelationship, fetchRoles } from '.';
 import { integrationConfig } from '../../../test/config';
 import { setupZoomRecording } from '../../../test/recording';
 import { fetchUsers } from '../users';
@@ -60,105 +60,112 @@ describe('#fetchRoles', () => {
   });
 });
 
-// describe('#buildUserAndGroupsRelationship', () => {
-//   let recording: Recording;
+describe('#buildUserAndRolesRelationship', () => {
+  let recording: Recording;
 
-//   beforeEach(() => {
-//     recording = setupZoomRecording({
-//       directory: __dirname,
-//       name: 'buildUserAndGroupsRelationship',
-//     });
-//   });
+  beforeEach(() => {
+    recording = setupZoomRecording({
+      directory: __dirname,
+      name: 'buildUserAndRolesRelationship',
+    });
+  });
 
-//   afterEach(async () => {
-//     await recording.stop();
-//   });
+  afterEach(async () => {
+    await recording.stop();
+  });
 
-//   test('should build group and user relationship', async () => {
-//     const context = createMockStepExecutionContext<IntegrationConfig>({
-//       instanceConfig: integrationConfig,
-//     });
+  test('should build role and user relationship', async () => {
+    const context = createMockStepExecutionContext<IntegrationConfig>({
+      instanceConfig: integrationConfig,
+    });
 
-//     await fetchUsers(context);
-//     await fetchGroups(context);
-//     await buildUserAndGroupsRelationship(context);
+    await fetchUsers(context);
+    await fetchRoles(context);
+    await buildUserAndRolesRelationship(context);
 
-//     expect({
-//       numCollectedEntities: context.jobState.collectedEntities.length,
-//       collectedEntities: context.jobState.collectedEntities,
-//       encounteredTypes: context.jobState.encounteredTypes,
-//       collectedRelationships: context.jobState.collectedRelationships,
-//     }).toMatchSnapshot();
+    expect({
+      numCollectedEntities: context.jobState.collectedEntities.length,
+      collectedEntities: context.jobState.collectedEntities,
+      encounteredTypes: context.jobState.encounteredTypes,
+      collectedRelationships: context.jobState.collectedRelationships,
+    }).toMatchSnapshot();
 
-//     const users = context.jobState.collectedEntities.filter((e) =>
-//       e._type.includes('zoom_user'),
-//     );
-//     expect(users.length).toBeGreaterThan(0);
-//     expect(users).toMatchGraphObjectSchema({
-//       _class: ['User'],
-//       schema: {
-//         additionalProperties: false,
-//         properties: {
-//           _rawData: {
-//             type: 'array',
-//             items: { type: 'object' },
-//           },
-//           _type: { const: 'zoom_user' },
-//           name: { type: 'string' },
-//           username: { type: 'string' },
-//           firstName: { type: 'string' },
-//           lastName: { type: 'string' },
-//           email: { type: 'string' },
-//           type: { type: 'number' },
-//           pmi: { type: 'number' },
-//           timezone: { type: 'string' },
-//           verified: { type: 'number' },
-//           createdAt: { type: 'string' },
-//           lastLoginTime: { type: 'string' },
-//           lastClientVersion: { type: 'string' },
-//           picUrl: { type: 'string' },
-//           language: { type: 'string' },
-//           phoneNumber: { type: 'string' },
-//           status: { type: 'string' },
-//           roleId: { type: 'string' },
-//         },
-//       },
-//     });
+    const users = context.jobState.collectedEntities.filter((e) =>
+      e._type.includes('zoom_user'),
+    );
+    expect(users.length).toBeGreaterThan(0);
+    expect(users).toMatchGraphObjectSchema({
+      _class: ['User'],
+      schema: {
+        additionalProperties: false,
+        properties: {
+          _rawData: {
+            type: 'array',
+            items: { type: 'object' },
+          },
+          _type: { const: 'zoom_user' },
+          name: { type: 'string' },
+          username: { type: 'string' },
+          firstName: { type: 'string' },
+          lastName: { type: 'string' },
+          email: { type: 'string' },
+          type: { type: 'number' },
+          pmi: { type: 'number' },
+          timezone: { type: 'string' },
+          verified: { type: 'number' },
+          createdAt: { type: 'string' },
+          lastLoginTime: { type: 'string' },
+          lastClientVersion: { type: 'string' },
+          picUrl: { type: 'string' },
+          language: { type: 'string' },
+          phoneNumber: { type: 'string' },
+          status: { type: 'string' },
+          roleId: { type: 'string' },
+          dept: { type: 'string' },
+          groupIds: {
+            type: 'array',
+            items: { type: 'string' },
+          },
+          hostKey: { type: 'string' },
+        },
+      },
+    });
 
-//     const groups = context.jobState.collectedEntities.filter((e) =>
-//       e._type.includes('zoom_group'),
-//     );
-//     expect(groups.length).toBeGreaterThan(0);
-//     expect(groups).toMatchGraphObjectSchema({
-//       _class: ['Group'],
-//       schema: {
-//         additionalProperties: false,
-//         properties: {
-//           _rawData: {
-//             type: 'array',
-//             items: { type: 'object' },
-//           },
-//           _type: { const: 'zoom_group' },
-//           id: { type: 'string' },
-//           name: { type: 'string' },
-//           totalMembers: { type: 'number' },
-//         },
-//       },
-//     });
+    const roles = context.jobState.collectedEntities.filter((e) =>
+      e._type.includes('zoom_role'),
+    );
+    expect(roles.length).toBeGreaterThan(0);
+    expect(roles).toMatchGraphObjectSchema({
+      _class: ['AccessRole'],
+      schema: {
+        additionalProperties: false,
+        properties: {
+          _rawData: {
+            type: 'array',
+            items: { type: 'object' },
+          },
+          _type: { const: 'zoom_role' },
+          id: { type: 'string' },
+          name: { type: 'string' },
+          totalMembers: { type: 'number' },
+          description: { type: 'string' },
+        },
+      },
+    });
 
-//     expect(
-//       context.jobState.collectedRelationships.filter(
-//         (e) => e._type === Relationships.GROUP_HAS_USER._type,
-//       ),
-//     ).toMatchDirectRelationshipSchema({
-//       schema: {
-//         properties: {
-//           _class: { const: 'HAS' },
-//           _type: {
-//             const: 'zoom_group_has_zoom_user',
-//           },
-//         },
-//       },
-//     });
-//   });
-// });
+    expect(
+      context.jobState.collectedRelationships.filter(
+        (e) => e._type === Relationships.GROUP_HAS_USER._type,
+      ),
+    ).toMatchDirectRelationshipSchema({
+      schema: {
+        properties: {
+          _class: { const: 'HAS' },
+          _type: {
+            const: 'zoom_user_assigned_role',
+          },
+        },
+      },
+    });
+  });
+});
