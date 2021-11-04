d41a7bca482a73b7ff4f29159d2b852e58251aea
Stefan Virag <viragsf@gmail.com>
2021-11-05 00:31:35 +0100

    Add getStepStartStates, small refactor

--- test/config.ts
+++ test/config.ts
@@ -8,7 +8,9 @@ if (process.env.LOAD_ENV) {
   });
 }
 const DEFAULT_ZOOM_ACCESS_TOKEN = 'dummy-zoom-access-token';
+const DEFAULT_SCOPES = 'dummy-zoom-scopes-list';
 
 export const integrationConfig: IntegrationConfig = {
   zoomAccessToken: process.env.ZOOM_ACCESS_TOKEN || DEFAULT_ZOOM_ACCESS_TOKEN,
+  scopes: process.env.SCOPES || DEFAULT_SCOPES,
 };
